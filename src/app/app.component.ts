aef82618c4460a3bdf3183d08381b3d710a40a62
JO-SIG <jo.sig@hotmail.com>
2019-02-14 21:54:51 +0100

    Le cours Améliorez la structure du code avec les Services OpenClassoom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {AppareilService} from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isAuth = false;
+  // lastUpdate = new Date();
 
-  appareilOne ='Machine à laver';
-  appareilTwo = 'Frigo';
-  appareilThree = 'Ordinateur';
-
-  constructor(){
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
     setTimeout(
-      () =>{
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  appareils: any[];
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
         this.isAuth = true;
       }, 4000
     );
   }
-  onAllumer(){
-    console.log('on allume tout !');
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
+  onAllumer() {
+    return this.appareilService.onAllumer();
+  }
+
+  onEteindre() {
+    return this.appareilService.onEteindre();
   }
 }
--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {AppareilService} from '../services/appareil.service';
 
 @Component({
   selector: 'app-appareil',
@@ -9,14 +10,25 @@ export class AppareilComponent implements OnInit {
 
   @Input() appareilName: string;
   @Input() appareilStatus: string;
+  @Input() index: number;
 
-  constructor() { }
+  constructor(private appareilService: AppareilService) { }
 
   ngOnInit() {
   }
 
   getStatus() {
     return this.appareilStatus;
+  }
+  getColor() {
+    if(this.appareilStatus === 'allumé') {
+      return 'green';
+    } else if(this.appareilStatus === 'éteint') {
+      return 'red';
+    }
+  }
+  onSwitch() {
+    return this.appareilService.onSwitch(this.appareilStatus, this.index);
   }
 
 }
